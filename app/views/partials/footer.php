fb93bedfc58ad7f5a1f798fee1ead76110ca5f75
Vemark1101 <vemarkapostadero@gmail.com>
2026-03-27 04:41:44 -0700

    Create footer.php

---
+++ app/views/partials/footer.php
@@ -0,0 +1,5 @@
+<?php $extraScripts = $extraScripts ?? ''; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<?= $extraScripts ?>
+</body>
+</html>
