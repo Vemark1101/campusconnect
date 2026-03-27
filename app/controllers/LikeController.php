7a9cc7f09c360c7c8c7724ed6c27000283f10441
Vemark1101 <vemarkapostadero@gmail.com>
2026-03-27 05:38:52 -0700

    Update LikeController.php

--- app/controllers/LikeController.php
+++ app/controllers/LikeController.php
@@ -11,6 +11,10 @@ class LikeController {
         $this->likeModel = new LikeModel();
     }
 
+    private function setFlash($type, $message) {
+        $_SESSION['flash_' . $type] = $message;
+    }
+
     public function like() {
         if (!isset($_SESSION['user_id'])) {
             header("Location: index.php?action=login");
@@ -18,10 +22,12 @@ class LikeController {
         }
 
         if (isset($_GET['post_id'])) {
-            $post_id = $_GET['post_id'];
+            $post_id = (int) $_GET['post_id'];
             $user_id = $_SESSION['user_id'];
 
-            if ($this->likeModel->alreadyLiked($post_id, $user_id)) {
+            if ($post_id <= 0) {
+                $this->setFlash('error', 'Invalid post selected.');
+            } elseif ($this->likeModel->alreadyLiked($post_id, $user_id)) {
                 $this->likeModel->unlike($post_id, $user_id);
             } else {
                 $this->likeModel->like($post_id, $user_id);
